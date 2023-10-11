844ad08dc354b07c4bc084d54914fda3c4e53081
Lieven <lieven@nrds.be>
2023-10-11 13:17:28 +0200

    removing temp solutions

---
+++ 5DAD/Example_Random/Example_Random/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example_Random
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int getalTussen0En101 = rand.Next(102);
+            int getalTussen0En21 = rand.Next(22);
+            
+            Console.WriteLine(getalTussen0En21 + getalTussen0En101);
+            
+        }
+    }
+}
