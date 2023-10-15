503cc5fd23cdbbd812f4818ba87261e99804cc25
lieven <lieven@karamel.one>
2023-10-15 10:44:39 +0200

    toevoegen extra oefeningen

---
+++ 5DAD/Oefeningen/Oefening_Espressomachine/Oefening_Espressomachine/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_Espressomachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxAantalbonen = 200;
+            int aantalBonen = 200;
+            bool inWerking = true;
+
+            int bonenPerMild = 20;
+            int bonenPerNormaal = 30;
+            int bonenPerSterk = 50;
+
+            Console.WriteLine("Espressomachine");
+            Console.WriteLine("Deze machine heeft 3 standen: mild, normaal en sterk. Geef X in om het programma te stoppen");
+            Console.WriteLine("Code\t\tsterkte");
+            Console.WriteLine("M\t\tMild\r\nN\t\tNormaal\r\nS\t\tSterk");
+
+            while (inWerking)
+            {
+                bool vulModus = false;
+                Console.WriteLine("Aantal bonen in de machine: " + aantalBonen);
+
+                Console.Write("Geef uw keuze voor de sterkte van uw kopje koffie in:");
+                string code = Console.ReadLine().ToUpper();
+
+                switch (code)
+                {
+                    case "M":
+                        if (aantalBonen >= bonenPerMild)
+                        {
+                            aantalBonen = aantalBonen - bonenPerMild;
+                        }
+                        else
+                        {
+                            vulModus = true;
+                        }
+                        break;
+                    case "N":
+                        if (aantalBonen >= bonenPerNormaal)
+                        {
+                            aantalBonen = aantalBonen - bonenPerNormaal;
+                        }
+                        else
+                        {
+                            vulModus = true;
+                        }
+                        break;
+                    case "S":
+                        if (aantalBonen >= bonenPerSterk)
+                        {
+                            aantalBonen = aantalBonen - bonenPerSterk;
+                        }
+                        else
+                        {
+                            vulModus = true;
+                        }
+                        break;
+                    case "X":
+                        Console.WriteLine("We sluiten de machine af");
+                        inWerking = false;
+                        break;
+                    default:
+                        Console.WriteLine("Kon sterkte niet bepalen");
+                        continue;
+                        break;
+                }
+                if (vulModus)
+                {
+                    Console.WriteLine("De bonen zijn op, U dient de machine aan te vullen");
+
+                }
+                while (vulModus)
+                {
+                    Console.WriteLine("wilt u bijvullen, typ 'JA' om de machine volledig bij te vullen");
+                    string input = Console.ReadLine().ToUpper();
+                    if (input == "JA")
+                    {
+                        aantalBonen = maxAantalbonen;
+                        vulModus = false;
+                        Console.WriteLine("De bonen werden aangevuld");
+                    }
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5DAD/Oefeningen/Oefening_WetGroteGetallen/Oefening_WetGroteGetallen/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_WetGroteGetallen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool inwerking = true;
+            Random rand = new Random();
+
+
+            Console.WriteLine("Wet van de grote getallen");
+            Console.WriteLine("-------------------------");
+
+
+            while (inwerking)
+            {
+                int aantalKeer = 0;
+                int aantalkeerMunt = 0;
+                Console.WriteLine("Geef een aantal in, ons programma zal dit aantal worpen simuleren.");
+                string input = Console.ReadLine().ToUpper();
+                if (input == "X")
+                {
+                    break;
+                }
+
+                aantalKeer = Convert.ToInt32(input);
+                for (int i = 0; i < aantalKeer; i++)
+                {
+                    //1 is munt, 2 is kop
+
+                    int worp = rand.Next(1, 3);
+                    if (worp == 1)
+                    { 
+                    aantalkeerMunt++;
+                    }
+                }
+
+                double percentage = Convert.ToDouble(aantalkeerMunt)/ Convert.ToDouble(aantalKeer) *100;
+                Console.WriteLine("Aantal keer geworpen: " + aantalKeer);
+                Console.WriteLine("Aantal keer Munt geworpen: " + aantalkeerMunt);
+                Console.WriteLine("percentage munt geworpen: " + percentage + "% \r\n");
+
+            }
+            Console.WriteLine("Het programma wordt afgesloten");
+            Console.ReadLine();
+        }
+    }
+}
